4944285a9ea46895ec5726727ed260c9d5427025
rs517 <rs517.learning@gmail.com>
2022-06-21 23:00:19 +0900

    first commit

---
+++ src/abc254_a.py
@@ -0,0 +1,12 @@
+import io
+import sys
+
+# input here
+# _INPUT = """\
+# 254
+# """
+# sys.stdin = io.StringIO(_INPUT)
+
+# your code here
+tar = list(input())
+print(tar[-2]+tar[-1])
---
+++ src/abc254_b.py
@@ -0,0 +1,17 @@
+import io
+import sys
+
+# input here
+_INPUT = """\
+3
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+# your code here
+tar = int(input())
+tar_list = []
+
+for i in range(tar):
+    tar_list.append(i)
+
+pass
